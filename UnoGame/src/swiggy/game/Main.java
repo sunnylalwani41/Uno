84321c2ebeca8e5cd5b8c86d8f38a5d30bdf29dc
sunnylalwani41 <sunnylalwani41@gmail.com>
2023-05-31 00:39:16 +0530

    uno

---
+++ UnoGame/src/swiggy/game/Main.java
@@ -0,0 +1,29 @@
+package swiggy.game;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import swiggy.model.Game;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Game game= new Game();
+		List<String> players= new ArrayList<>();
+		int i=1;
+		while(true) {
+			System.out.println("Enter "+ i +" player name");
+			players.add(sc.next());
+			if(i>=2) {
+				System.out.println("There have more player(s):- yes/no");
+				String input= sc.next();
+				if(input.equalsIgnoreCase("no")) {
+					break;
+				}
+			}
+		}
+		game.startTheGame(players);
+		
+	}
+}
